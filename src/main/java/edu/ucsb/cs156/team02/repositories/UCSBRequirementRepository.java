a2b65f6a2569ca75fc96e166f89847b5ba46e940
LucienLuc <lucienluc@umail.ucsb.edu>
2022-02-02 12:54:57 -0800

    Fix issue with wrong method name

--- src/main/java/edu/ucsb/cs156/team02/repositories/UCSBRequirementRepository.java
+++ src/main/java/edu/ucsb/cs156/team02/repositories/UCSBRequirementRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UCSBRequirementRepository extends CrudRepository<UCSBRequirement, Long>{
-    Iterable<UCSBRequirement> findBySubject(String subject);
+    Iterable<UCSBRequirement> findByRequirementCode(String requirementCode);
 }
