f739c9891b203a8afdd275d272685b6eb55cea7d
Luke Yoffe <lukeyoffe@umail.ucsb.edu>
2022-02-07 13:29:14 -0800

    made collegiateSubredditController

---
+++ src/main/java/edu/ucsb/cs156/team02/controllers/CollegiateSubredditController.java
@@ -0,0 +1,58 @@
+package edu.ucsb.cs156.team02.controllers;
+
+import edu.ucsb.cs156.team02.entities.CollegiateSubreddit;
+import edu.ucsb.cs156.team02.models.CurrentUser;
+import edu.ucsb.cs156.team02.models.SystemInfo;
+import edu.ucsb.cs156.team02.repositories.CollegiateSubredditRepository;
+import edu.ucsb.cs156.team02.services.SystemInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(description = "API Information for Collegiate Subreddits")
+@RequestMapping("/api/collegiateSubreddits")
+@RestController
+@Slf4j
+public class CollegiateSubredditController extends ApiController {
+
+    @Autowired
+    CollegiateSubredditRepository collegiateSubredditRepository;
+
+    @ApiOperation(value = "Returns list of all subreddits in the database")
+    @GetMapping("all")
+    public Iterable<CollegiateSubreddit> allCollegiateSubreddits(){
+        loggingService.logMethod();
+        Iterable<CollegiateSubreddit> subreddits = collegiateSubredditRepository.findAll();
+        return subreddits;
+    }
+
+    @ApiOperation(value = "Creates a new subreddit")
+    @PostMapping("post")
+    public CollegiateSubreddit postCollegiateSubreddit(
+            @ApiParam("name") @RequestParam String name,
+            @ApiParam("location") @RequestParam String location,
+            @ApiParam("subreddit") @RequestParam String subreddit){
+        
+        loggingService.logMethod();
+        CurrentUser currentUser = getCurrentUser();
+        log.info("currentUser={}", currentUser);
+
+        CollegiateSubreddit collegiateSubreddit = new CollegiateSubreddit();
+        collegiateSubreddit.setName(name);
+        collegiateSubreddit.setLocation(location);
+        collegiateSubreddit.setSubreddit(subreddit);
+
+        CollegiateSubreddit savedCollegiateSubreddit = collegiateSubredditRepository.save(collegiateSubreddit);
+        return savedCollegiateSubreddit;
+    }
+
+}
