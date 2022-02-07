0ec1b2ac7b8096276058cfa831aa9737e6ee79ac
LucienLuc <lucienluc@umail.ucsb.edu>
2022-02-07 12:38:42 -0800

    Create file UCSBRequirementController and /all endpoint

---
+++ src/main/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementController.java
@@ -0,0 +1,59 @@
+package edu.ucsb.cs156.team02.controllers;
+
+import edu.ucsb.cs156.team02.entities.UCSBRequirement;
+import edu.ucsb.cs156.team02.entities.User;
+import edu.ucsb.cs156.team02.models.CurrentUser;
+import edu.ucsb.cs156.team02.repositories.UCSBRequirementRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Api(description = "API for UCSBRequirements")
+@RequestMapping("/api/UCSBRequirements")
+@RestController
+@Slf4j
+public class UCSBRequirementController extends ApiController{
+    
+    public class UCSBRequirementOrError {
+        Long id;
+        UCSBRequirement ucsbRequirement;
+        ResponseEntity<String> error;
+
+        public UCSBRequirementOrError(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Autowired
+    UCSBRequirementRepository ucsbRequirementRepository;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @ApiOperation(value = "List all UCSBRequirements")
+    @GetMapping("/all")
+    public Iterable<UCSBRequirement> allUcsbRequirements() {
+        loggingService.logMethod();
+        Iterable<UCSBRequirement> requirements = ucsbRequirementRepository.findAll();
+        return requirements;
+    }
+}
